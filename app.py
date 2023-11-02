bef502558fd87e1fd7f6fbab187a1e3f1ede00da
unknown <danishali6421@gmail.com>
2023-11-03 00:24:53 +0500

    app functionality

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def home():
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    os.system("dvc repro")
+    os.system("python main.py")
     return "Training done successfully!"
 
 
@@ -43,6 +43,6 @@ def predictRoute():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080) #local host
+    app.run(host='0.0.0.0', port=80) #local host
     # app.run(host='0.0.0.0', port=8080) #for AWS
     #app.run(host='0.0.0.0', port=80) #for AZURE
